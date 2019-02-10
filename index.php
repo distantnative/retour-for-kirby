f58c36feff1c6bf1e26cecad6c9ebbf6220826ec
Nico Hoffmann <on-github@nhoffmann.com>
2019-02-10 19:03:04 +0100

    Simplify code files

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
-require 'load.php';
+load([
+    'distantnative\\Retour\\Retour'    => 'models/Retour.php',
+    'distantnative\\Retour\\Log'       => 'models/Log.php',
+    'distantnative\\Retour\\Logs'      => 'models/Logs.php',
+    'distantnative\\Retour\\Redirects' => 'models/Redirects.php',
+    'distantnative\\Retour\\Stats'     => 'models/Stats.php',
+    'distantnative\\Retour\\System'    => 'models/System.php'
+], __DIR__);
 
 $retour = new distantnative\Retour\Retour;
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,12 @@ namespace distantnative\Retour;
 
 require __DIR__ . '/../../../../kirby/bootstrap.php';
 require __DIR__ . '/../load.php';
-require 'TestCase.php';
+
+load([
+    'distantnative\\Retour\\Retour'    => 'models/Retour.php',
+    'distantnative\\Retour\\Log'       => 'models/Log.php',
+    'distantnative\\Retour\\Logs'      => 'models/Logs.php',
+    'distantnative\\Retour\\Redirects' => 'models/Redirects.php',
+    'distantnative\\Retour\\Stats'     => 'models/Stats.php',
+    'distantnative\\Retour\\System'    => 'models/System.php'
+], dirname(__DIR__));
