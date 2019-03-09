18eb6da5b07b36404ccf8ab74eb04ac7e408e575
Sylvain Julé <sylvain.jule@free.fr>
2019-03-09 17:45:56 +0100

    Add French translation

---
+++ config/translations/fr.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'view.retour' => 'Retour',
+    'rt.refresh'  => 'Actualiser',
+
+    'rt.dashboard'       => 'Tableau de bord',
+    'rt.dashboard.month' => 'Mois',
+    'rt.dashboard.week'  => 'Semaine',
+    'rt.dashboard.day'   => 'Jour',
+
+    'rt.redirects'                 => 'Redirections',
+    'rt.redirects.from'            => 'Modèle d\'URL',
+    'rt.redirects.from.help'       => 'Ajoutez <a href="{reference}" target="_blank">des placeholders dynamiques</a> et <a href="{readme}" target="_blank">utilisez-les</a> dans vos redirections.',
+    'rt.redirects.to'              => 'Rediriger vers',
+    'rt.redirects.to.help'         => 'Chemin relatif ou URL absolue. Laisser vide pour avec le code de statut.',
+    'rt.redirects.status'          => 'Statut',
+    'rt.redirects.status.help'     => 'Découvrir <a href="{url}" target="_blank">les codes de statut HTTP</a> et quand les utiliser.',
+    'rt.redirects.hits'            => 'Requêtes traitées',
+    'rt.redirects.hit.last'        => 'Dernière',
+    'rt.redirects.state'           => 'Redirigée(s)',
+
+    'rt.fails'             => 'Échecs',
+    'rt.fails.path'        => 'Chemin',
+    'rt.fails.referrer'    => 'Référent',
+    'rt.fails.state'       => 'Échec(s)',
+    'rt.fails.last'        => 'Dernier',
+
+    'rt.settings'                   => 'Réglages',
+    'rt.settings.headline'          => 'Gestionnaire des redirections pour Kirby 3',
+    'rt.settings.installed'         => 'Version installée',
+    'rt.settings.latest'            => 'Dernière version',
+    'rt.settings.download'          => 'Télécharger <a href="{url}"target="_blank">la dernière version</a>.',
+    'rt.settings.support'           => 'Soutenir le développement',
+    'rt.settings.support.juice'     => 'Acheter un jus',
+    'rt.settings.support.donate'    => 'Donation',
+    'rt.settings.support.affiliate' => 'Acheter une license Kirby',
+    'rt.settings.overview'          => 'Vue d\'ensemble',
+    'rt.settings.log.clear'         => 'Vider le log',
+    'rt.settings.log.clear.confirm' => 'Voulez-vous vraiment vider le log?',
+    'rt.settings.routes'            => 'Redirections paramétrées',
+    'rt.settings.fails'             => 'Échecs détectés',
+    'rt.settings.redirects'         => 'Redirigées avec succès',
+    'rt.settings.docs'              => 'Retrouvez toutes les options <a href="{url}">dans la documentation</a>.'
+
+];
