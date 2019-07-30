4a30d252b895a5198e58076733b71bb1108299bd
Ahmet Bora <byybora@gmail.com>
2019-07-30 10:58:28 +0300

    Create tr.php

---
+++ config/translations/tr.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'view.retour'                   => 'Yönlendirme',
+    'rt.refresh'                    => 'Yenile',
+
+    'rt.tbl.all'                    => 'Tümü',
+    'rt.tbl.reset'                  => 'Sıfırla',
+    'rt.tbl.redirects.filter'       => 'Filtrele...',
+    'rt.tbl.redirects.perPage'      => 'Yönlendirme gösteriliyor',
+    'rt.tbl.redirects.empty'        => 'Yönlendirme yok',
+    'rt.tbl.fails.filter'           => 'Filtrele...',
+    'rt.tbl.fails.perPage'          => 'Başarısızlar gösteriliyor',
+    'rt.tbl.fails.empty'            => 'Başarısız yok',
+
+    'rt.dashboard'                  => 'Gösterge',
+    'rt.dashboard.month'            => 'Ay',
+    'rt.dashboard.week'             => 'Hafta',
+    'rt.dashboard.day'              => 'Gün',
+
+    'rt.redirects'                  => 'Yönlendirmeler',
+    'rt.redirects.from'             => 'URL deseneni',
+    'rt.redirects.from.help'        => '<a href="{reference}" target="_blank">Dinamik yer tutucuları</a> ekleyin ve yönlendirmede <a href="{readme}" target="_blank">kullanın</a>.',
+    'rt.redirects.to'               => 'Adresine yönlendir',
+    'rt.redirects.to.help'          => 'Site içi veya tam bir adres girin. İsteğin durum kodu ile tamamlanması için boş bırakın.',
+    'rt.redirects.status'           => 'Durum',
+    'rt.redirects.status.help'      => '<a href="{url}" target="_blank">HTTP durum kodları</a> ve hangisini ne zaman kullanılacağı hakkında bilgi edinin.',
+    'rt.redirects.hits'             => 'İsabet',
+    'rt.redirects.hit.last'         => 'Son',
+    'rt.redirects.state'            => 'Yönlendirildi',
+
+    'rt.fails'                      => 'Başarısızlar',
+    'rt.fails.path'                 => 'Yol',
+    'rt.fails.referrer'             => 'Yönlendiren',
+    'rt.fails.state'                => 'Başarısız',
+    'rt.fails.last'                 => 'Son',
+
+    'rt.settings'                   => 'Ayarlar',
+    'rt.settings.headline'          => 'Kirby 3 için gelişmiş yönlendirme yönetimi',
+    'rt.settings.installed'         => 'Yüklü versiyon',
+    'rt.settings.latest'            => 'Son versiyon',
+    'rt.settings.download'          => '<a href="{url}"target="_blank">Son versiyonu</a> indir.',
+    'rt.settings.support'           => 'Destek geliştirme',
+    'rt.settings.support.juice'     => 'Meyve suyu al',
+    'rt.settings.support.donate'    => 'Bağış Yap',
+    'rt.settings.support.affiliate' => 'Kirby lisansı satın al',
+    'rt.settings.overview'          => 'Genel Bakış',
+    'rt.settings.log.clear'         => 'Günlük kayıtları temizle',
+    'rt.settings.log.clear.confirm' => 'Günlük kayıtlarını gerçekten silmek istiyor musunuz?',
+    'rt.settings.routes'            => 'Yönlendirme rotaları',
+    'rt.settings.fails'             => 'Başarısız günlük kayıtları',
+    'rt.settings.redirects'         => 'Başarıyla yönlendirildi',
+    'rt.settings.docs'              => '<a href="{url}">Dokümanlardaki</a> seçenekler hakkında daha fazla bilgi edinin.'
+];
