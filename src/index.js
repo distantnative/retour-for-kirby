5f4c905a73acab35d233fadc1bc13760086008ee
Nico Hoffmann <on-github@nhoffmann.com>
2019-10-18 20:32:31 +0200

    Raise requirement to Kirby 3.3 and bump version

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-
-
+// Vue components
 import View from "./components/View.vue";
 import RedirectField from "./components/Fields/Redirect.vue";
 import StatusField from "./components/Fields/Status.vue";
 
+const view = {
+  component: View,
+  icon: "retour"
+};
+
+const icon = '<use xlink:href="#icon-undo" transform="translate(7.5,7.5) rotate(170) translate(-7.5,-7.5)"></use>';
+
+// Vuex store
 import store from "./store/retour.js";
 
+// 3rd party assets
 import "tbl-for-kirby/index.css";
 import "./assets/chart.css";
 
+// Register everything
 panel.plugin("distantnative/retour", {
-  views: {
-    retour: {
-      component: View,
-      icon: "retour"
-    }
-  },
-  icons: {
-    retour: '<use xlink:href="#icon-undo" transform="translate(7.5,7.5) rotate(170) translate(-7.5,-7.5)"></use>'
-  },
+  views: { retour: view },
+  icons: { retour: icon },
   fields: {
     "rt-redirect": RedirectField,
     "rt-status": StatusField
   },
   components: {
     "k-rt-redirect-input": { extends: "k-text-input" },
-    "k-rt-status-input": { extends: "k-select-input" },
+    "k-rt-status-input":   { extends: "k-select-input" },
   },
   created(app) {
     app.$store.registerModule("retour", store);
