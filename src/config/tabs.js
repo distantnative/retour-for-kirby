08624edba820058bac9b6d790741187ad7f466ad
Nico Hoffmann <on-github@nhoffmann.com>
2020-06-14 17:49:50 +0200

    🚚 Move some display logic to tabs config

--- src/config/tabs.js
+++ src/config/tabs.js
@@ -1,24 +1,30 @@
 
 export default (Vue) => {
-  const store    = Vue.$store.state.retour;
-  const routes   = store.data.routes.length;
-  const failures = store.data.failures.length;
+  let tabs    = [];
+  const store = Vue.$store.state.retour;
 
-  if (failures > 1000) {
-    failures = (Math.floor(failures / 100) / 10) + "k";
-  }
+  // routes
+  const routes = store.data.routes.length;
 
-  return [
-    {
-      name: "routes",
-      label: Vue.$t("retour.routes"),
-      icon: "undo",
-      badge: routes ? {
-        count: routes,
-        color: "focus"
-      }: false
-    },
-    {
+  tabs.push({
+    name: "routes",
+    label: Vue.$t("retour.routes"),
+    icon: "undo",
+    badge: routes ? {
+      count: routes,
+      color: "focus"
+    }: false
+  });
+
+  // failures
+  if (store.system.hasLog) {
+    const failures = store.data.failures.length;
+
+    if (failures > 1000) {
+      failures = (Math.floor(failures / 100) / 10) + "k";
+    }
+
+    tabs.push({
       name: "failures",
       label: Vue.$t("retour.failures"),
       icon: "alert",
@@ -26,15 +32,19 @@ export default (Vue) => {
         count: failures,
         color: "negative"
       } : false
-    },
-    {
-      name: "system",
-      label: Vue.$t("retour.system"),
-      icon: "box",
-      badge: store.system.update < 0 ? {
-        count: 1,
-        color: "positive"
-      } : false
-    }
-  ];
+    });
+  }
+
+  // system
+  tabs.push({
+    name: "system",
+    label: Vue.$t("retour.system"),
+    icon: "box",
+    badge: store.system.update < 0 ? {
+      count: 1,
+      color: "positive"
+    } : false
+  });
+
+  return tabs;
 }
