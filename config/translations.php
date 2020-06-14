fa412ce0dfae48c593352e4eac0e8e1807ad4cc7
Nico Hoffmann <on-github@nhoffmann.com>
2020-06-14 12:41:06 +0200

    🌎 Add Spanish i18n file #176

--- config/translations.php
+++ config/translations.php
@@ -2,6 +2,7 @@
 
 return [
     'en'    => require 'i18n/en.php',
+    'es_ES' => require 'i18n/es_ES.php',
     'de'    => require 'i18n/de.php',
     'fr'    => require 'i18n/fr.php',
     'pt_BR' => require 'i18n/pt_BR.php',
