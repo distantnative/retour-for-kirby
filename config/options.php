52297b1f89bf4996786a28a838fd11d601204855
Nico Hoffmann <on-github@nhoffmann.com>
2020-06-14 22:09:29 +0200

    🚧 Draft for tracking feature backend code

--- config/options.php
+++ config/options.php
@@ -7,6 +7,8 @@ $site = dirname(__DIR__, 3);
 return [
     'api'         => true,
     'logs'        => true,
+    'tracking'    => false,
+    'deletions'   => false,
     'ignore'      => [],
     'config'      => $site . '/config/redirects.yml',
     'database'    => $site . '/logs/retour.sqlite',
--- index.js
+++ index.js
@@ -174,16 +174,19 @@ var _default = Vue => {
     } : false
   }); // tracked routes
 
-  const tracked = store.data.tracked.filter(route => route.active === false).length;
-  tabs.push({
-    name: "tracked",
-    label: "Tracked",
-    icon: "live",
-    badge: tracked ? {
-      count: tracked,
-      color: "yellow"
-    } : false
-  }); // failures
+  if (store.system.hasTracking) {
+    const tracked = store.data.tracked.filter(route => route.active === false).length;
+    tabs.push({
+      name: "tracked",
+      label: Vue.$t("retour.tracked"),
+      icon: "live",
+      badge: tracked ? {
+        count: tracked,
+        color: "yellow"
+      } : false
+    });
+  } // failures
+
 
   if (store.system.hasLog) {
     const failures = store.data.failures.length;
@@ -15296,9 +15299,12 @@ var _default = {
     },
 
     async onCellUpdate(rows) {
-      console.log(rows); //row[columnIndex] = value;
-      //rows.splice(rowIndex, 1, row);
-      //await this.onUpdate(rows);
+      // TODO: see if that's a bug in Kirby itself
+      if (Array.isArray(rows) === false) {
+        return;
+      }
+
+      await this.onUpdate(rows);
     },
 
     async onEdit() {
@@ -15310,9 +15316,15 @@ var _default = {
     },
 
     onOption(option, row = {}, rowIndex) {
-      this.row = this.$helper.clone(row);
-      this.rowIndex = rowIndex;
-      this.$refs[option + "Dialog"].open();
+      switch (option) {
+        case "move":
+          return this.$events.$emit("retour.move", row);
+
+        default:
+          this.row = this.$helper.clone(row);
+          this.rowIndex = rowIndex;
+          this.$refs[option + "Dialog"].open();
+      }
     },
 
     async onRemove() {
@@ -15320,18 +15332,6 @@ var _default = {
       rows.splice(this.rowIndex, 1);
       await this.onUpdate(rows);
       this.$refs.removeDialog.close();
-    },
-
-    async onResolve() {
-      try {
-        await this.$api.post("retour/log/resolve", {
-          path: this.row.from
-        });
-        const calls = [this.$store.dispatch("retour/failures"), this.$store.dispatch("retour/stats")];
-        await Promise.all(calls);
-      } catch (error) {
-        this.$store.dispatch("notification/error", error);
-      }
     },
 
     async onUpdate(rows) {
@@ -15342,8 +15342,10 @@ var _default = {
         await this.$store.dispatch("retour/routes", this.type);
 
         if (this.after) {
-          await this.after();
+          await this.after(this.row);
         }
+
+        this.$store.dispatch("notification/success");
       } catch (error) {
         this.$store.dispatch("notification/error", error);
       } finally {
@@ -15352,9 +15354,10 @@ var _default = {
       }
     },
 
-    resolve(row) {
+    insert(row, after) {
+      row.active = true;
+      this.after = after;
       this.onOption("add", row);
-      this.after = this.onResolve;
     }
 
   }
@@ -15525,6 +15528,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 //
 //
 //
+//
 var _default = {
   mixins: [_permissions.default],
   components: {
@@ -15552,15 +15556,43 @@ var _default = {
   },
 
   created() {
+    // this.$events.$on("retour.move", this.move);
     this.$events.$on("retour.resolve", this.resolve);
   },
 
   destroyed() {
+    // this.$events.$off("retour.move", this.move);
     this.$events.$off("retour.resolve", this.resolve);
   },
 
   methods: {
-    onResolve() {}
+    // move(row) {
+    //   this.$refs.table.insert(row, this.onMove);
+    // },
+    resolve(row) {
+      this.$refs.table.insert(row, this.onResolve);
+    },
+
+    // async onMove(row) {
+    //   try {
+    //     // remove route from tracked
+    //     await this.$store.dispatch("retour/routes", "tracked")
+    //     await Promise.all(calls);
+    //   } catch (error) {
+    //     this.$store.dispatch("notification/error", error);
+    //   }
+    // },
+    async onResolve(row) {
+      try {
+        await this.$api.post("retour/log/resolve", {
+          path: row.from
+        });
+        const calls = [this.$store.dispatch("retour/failures"), this.$store.dispatch("retour/stats")];
+        await Promise.all(calls);
+      } catch (error) {
+        this.$store.dispatch("notification/error", error);
+      }
+    }
 
   }
 };
@@ -15578,6 +15610,7 @@ exports.default = _default;
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("retour-routes-table", {
+    ref: "table",
     attrs: {
       canEdit: _vm.canUpdate,
       label: _vm.$t("retour.routes"),
@@ -16023,24 +16056,43 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 var _default = {
   mixins: [_permissions.default],
   components: {
     "retour-routes-table": _RoutesTable.default
   },
   computed: {
+    hasTracking() {
+      return this.$store.state.retour.system.hasTracking;
+    },
+
     options() {
       if (this.canUpdate !== false) {
-        return [{
-          text: this.$t("retour.tracked.add"),
-          icon: "parent",
-          click: "insert"
-        }, {
+        return [// { text: this.$t("retour.tracked.move"), icon: "parent", click: "insert" },
+        {
           text: this.$t("remove"),
           icon: "trash",
           click: "remove"
         }];
       }
+    },
+
+    recent() {
+      return this.rows.filter(row => row.active === false).length;
     },
 
     rows() {
@@ -16062,15 +16114,50 @@ exports.default = _default;
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("retour-routes-table", {
-    attrs: {
-      canEdit: false,
-      label: _vm.$t("retour.tracked"),
-      options: _vm.options,
-      rows: _vm.rows,
-      tab: "routes"
-    }
-  })
+  return _c(
+    "div",
+    [
+      _vm.hasTracking === false
+        ? [
+            _c("k-box", { attrs: { theme: "info" } }, [
+              _vm._v(
+                "\n      Tracking has not been enabled. To do so, please set the "
+              ),
+              _c("b", [_vm._v("distantnative.retour.tracking")]),
+              _vm._v(" option in your "),
+              _c("b", [_vm._v("site/config/config.php")]),
+              _vm._v(".\n    ")
+            ])
+          ]
+        : [
+            _vm.recent
+              ? _c(
+                  "k-box",
+                  { staticClass: "mb-6", attrs: { theme: "notice" } },
+                  [
+                    _c("b", [
+                      _vm._v(_vm._s(_vm.recent) + " inactive route(s)")
+                    ]),
+                    _vm._v(
+                      " for tracked changes. Either activate or dismiss by removing the route.\n    "
+                    )
+                  ]
+                )
+              : _vm._e(),
+            _vm._v(" "),
+            _c("retour-routes-table", {
+              attrs: {
+                canEdit: false,
+                label: _vm.$t("retour.tracked"),
+                options: _vm.options,
+                rows: _vm.rows,
+                type: "tracked"
+              }
+            })
+          ]
+    ],
+    2
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -16099,9 +16186,13 @@ render._withStripped = true
         }
 
         
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
       }
     })();
-},{"../mixins/permissions.js":"mixins/permissions.js","./RoutesTable.vue":"components/RoutesTable.vue","vue-hot-reload-api":"../node_modules/vue-hot-reload-api/dist/index.js","vue":"../node_modules/vue/dist/vue.runtime.esm.js"}],"components/View.vue":[function(require,module,exports) {
+},{"../mixins/permissions.js":"mixins/permissions.js","./RoutesTable.vue":"components/RoutesTable.vue","_css_loader":"../../../../../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"../node_modules/vue-hot-reload-api/dist/index.js","vue":"../node_modules/vue/dist/vue.runtime.esm.js"}],"components/View.vue":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -17386,6 +17477,7 @@ var _default = Vue => ({
     system: {
       deleteAfter: null,
       hasLog: false,
+      hasTracking: false,
       headers: [],
       release: null,
       version: null,
