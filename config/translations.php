c3640c5f2273abbb48b03b959017741d2ba85be5
Nico Hoffmann <on-github@nhoffmann.com>
2020-06-14 22:09:03 +0200

    Fix i18n loading

--- config/translations.php
+++ config/translations.php
@@ -3,13 +3,13 @@
 use Kirby\Toolkit\Dir;
 use Kirby\Toolkit\F;
 
-$translations= [];
+$translations = [];
 $root = __DIR__ . '/i18n';
 
 foreach (Dir::files($root) as $file) {
     $locale = basename($file, '.json');
     $content = F::read($root . '/' . $file);
-    $translations[] = json_decode($content, true);
+    $translations[$locale] = json_decode($content, true);
 }
 
 return $translations;
