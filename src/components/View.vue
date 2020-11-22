573cddd9a19acb3c9ba9a462442fa4cdffdfee93
Nico Hoffmann <on-github@nhoffmann.com>
2020-11-22 09:03:50 +0100

    ♻️ Refactor tabs bar

--- src/components/View.vue
+++ src/components/View.vue
@@ -1,39 +1,25 @@
 <template>
   <div class="rt-view">
-
-    <!-- loader -->
     <k-loader v-if="isLoading" />
 
     <template v-else>
-      <!-- stats -->
-      <retour-stats v-if="hasLog" />
-
-      <!-- tabs -->
-      <k-view class="rt-tabs">
-        <k-tabs :tabs="tabs" :tab="tab" />
-        <component :is="'retour-' + this.tab + '-tab'" />
-      </k-view>
+      <rt-stats v-if="hasLog" />
+      <rt-tabs />
     </template>
   </div>
 </template>
 
 <script>
 import permissions from "../mixins/permissions.js";
-import tabs from "../config/tabs.js";
 
 import Stats from "./Stats.vue";
-
-import FailuresTab from "./Tabs/FailuresTab.vue";
-import RoutesTab from "./Tabs/RoutesTab.vue";
-import SystemTab from "./Tabs/SystemTab.vue";
+import Tabs from "./Tabs.vue";
 
 export default {
   mixins: [permissions],
   components: {
-    "retour-stats": Stats,
-    "retour-failures-tab": FailuresTab,
-    "retour-routes-tab": RoutesTab,
-    "retour-system-tab": SystemTab
+    "rt-stats": Stats,
+    "rt-tabs": Tabs
   },
   data() {
     return {
@@ -43,21 +29,6 @@ export default {
   computed: {
     hasLog() {
       return this.$store.state.retour.system.hasLog;
-    },
-    tab() {
-      return this.$route.hash.slice(1) || "routes";
-    },
-    tabs() {
-      return tabs(this);
-    }
-  },
-  watch: {
-    "$route.hash": {
-      handler() {
-        const tab = this.tabs.filter(tab => tab.name === this.tab)[0];
-        this.$emit("breadcrumb", [{ text: tab.label }]);
-      },
-      immediate: true
     }
   },
   async created() {
@@ -81,8 +52,5 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-}
-.rt-tabs {
-  margin-top: 1.5rem;
 }
 </style>
