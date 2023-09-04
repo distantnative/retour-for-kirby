4628194d8fb968027276a22d73eb304670c54e2f
Nico Hoffmann <me@nhoffmann.com>
2023-09-04 22:46:11 +0200

    Fix colors mixin

--- src/panel/mixins/color.js
+++ src/panel/mixins/color.js
@@ -2,18 +2,18 @@ export default {
   computed: {
     color() {
       if (!this.value) {
-        return "var(--color-border)";
+        return "var(--color-gray-400)";
       }
 
       if (parseInt(this.value) >= 400) {
-        return "var(--color-negative-light)";
+        return "var(--color-red-500)";
       }
 
       if (parseInt(this.value) >= 300) {
-        return "var(--color-positive-light)";
+        return "var(--color-green-500)";
       }
 
-      return "var(--color-focus-light)";
+      return "var(--color-blue-500)";
     },
   },
 };
