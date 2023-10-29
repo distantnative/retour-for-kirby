8d7d60f33d4823d3d121f762148dfe17a0f3739e
Nico Hoffmann <me@nhoffmann.com>
2023-10-29 17:07:59 +0100

    Fix Page UUID support
    Fixes #327

--- src/panel/components/Table/StatusFieldPreview.vue
+++ src/panel/components/Table/StatusFieldPreview.vue
@@ -1,7 +1,10 @@
 <template>
-  <div :title="`${column.label}: ${value}`" class="k-status-field-preview">
+  <div
+    :title="`${column.label}: ${value}`"
+    class="k-text k-status-field-preview"
+  >
     <k-icon type="circle-filled" :style="'color: ' + color" />
-    <kbd>{{ value }}</kbd>
+    <code>{{ value }}</code>
   </div>
 </template>
 
@@ -20,12 +23,11 @@ export default {
 
 <style>
 .k-status-field-preview {
+  --code-inline-color-back: var(--color-blue-200);
+  --code-inline-color-border: var(--color-blue-400);
+
   display: flex;
   gap: var(--spacing-2);
   padding-inline: var(--spacing-2);
-}
-.k-status-field-preview kbd {
-  font-size: var(--text-xs);
-  font-family: var(--font-mono);
 }
 </style>
